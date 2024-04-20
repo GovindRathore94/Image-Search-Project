c8277f75e6ba6947236b1de4660134ef78a49724
Govind Singh Rathore <146634853+GovindRathore94@users.noreply.github.com>
2024-04-20 10:57:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let page = 1;
 
 async function searchImage(){
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`
+    const url = `http://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`
 
     const response = await fetch(url);
     const data = await response.json();
